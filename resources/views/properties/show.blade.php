75b06d7b2a724cc7f49d46741a7d1e849197bd74
imurar <imurar@outlook.com>
2025-07-16 14:29:07 +0900

    feat: add lat and lng

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1>{{ $property->name }}</h1>
     <div id="vue-property-map">
-        <property-map></property-map>
+        <property-map :lat="{{ property->latitude }}" :lng="{{ $property->longitude }}"></property-map>
     </div>
 @endsection
