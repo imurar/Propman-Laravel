2b39793eac1e7b9d83e4dfde0977fc809316dbc9
imurar <imurar@outlook.com>
2025-07-20 17:11:46 +0900

    feat: Property-model

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
+    protected $fillable = [
+        'name', 'address', 'latitude', 'longitude'
+    ];
+
     public function agents()
     {
-        return $this->belongsToMany(Agent::class, 'property_agent');
+        return $this->belongsToMany(Agent::class, 'property_agent')->withTimestamps();
     }
 }
