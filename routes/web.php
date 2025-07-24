19165a42f431b13927602170a9351f9475d74a3e
imurar <imurar@outlook.com>
2025-07-24 23:54:16 +0900

    refactor: Inertia-routing

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\AgentController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Inertiaページとしてルーティング
+Route::get('/', [PropertyController::class, 'index'])->name('home');
+Route::resource('properties', PropertyController::class)->except(['show']);
+Route::get('properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::resource('agents', AgentController::class)->except(['show']);
+Route::get('agents/{agent}', [AgentController::class, 'show'])->name('agents.show');
