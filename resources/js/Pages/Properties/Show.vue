df5000831d7aabfdbe9d42080c35dea9211b372d
imurar <imurar@outlook.com>
2025-07-25 16:18:46 +0900

    feat: create property show vue

---
+++ resources/js/Pages/Properties/Show.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import PropertyMap from "../../Components/PropertyMap.vue";
+defineProps({ property: Object });
+</script>
+
+<template>
+    <div>
+        <h2>{{ property.name }}</h2>
+        <p>住所: {{ property.address }}</p>
+        <p>緯度: {{ property.latitude }}, 経度: {{ property.longitude }}</p>
+        <PropertyMap
+            v-if="property.latitude && property.longitude"
+            :lat="property.latitude"
+            :lng="property.longitude"
+        />
+
+        <h3>担当者</h3>
+        <ul>
+            <li v-for="agent in property.agents" :key="agent.id">
+                {{ agent.name }}
+            </li>
+        </ul>
+        <Link href="/properties">一覧へ戻る</Link>
+    </div>
+</template>
