eab6f10aad087312897a480986a4c9b6d226e3e2
imurar <imurar@outlook.com>
2025-07-26 13:22:09 +0900

    feat: properties show.blade

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $property->name }}</h1>
-    <div id="vue-property-map">
-        <property-map :lat="{{ property->latitude }}" :lng="{{ $property->longitude }}"></property-map>
+<h2>物件詳細</h2>
+<p>物件名: {{ $property->name }}</p>
+<p>住所: {{ $property->address }}</p>
+<p>緯度: {{ $property->latitude }}, 経度: {{ $property->longitude }}</p>
+
+@if($property->latitude && $property->longitude)
+    <div id="vue-property-map" style="height:400px;">
+        <property-map :lat="{{ $property->latitude }}" :lng="{{ $property->longitude }}"></property-map>
     </div>
+@endif
+
+<h3>担当者割り当て</h3>
+<div id="vue-agent-assign">
+    <agent-assign :property-id="{{ $property->id }}" :agents="{{ json_encode($property->agents) }}"></agent-assign>
+</div>
 @endsection
