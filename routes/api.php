255862c96f2d954d121cc3746e2641ad488b0065
imurar <imurar@outlook.com>
2025-07-26 15:47:09 +0900

    fix: create agents index api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyAgentController;
+use App\Http\Controllers\AgentController;
 
 // 物件と担当者の割り当て
 Route::post('/properties/{property}/assign', [PropertyAgentController::class, 'assign']);
 Route::post('/properties/{property}/unassign', [PropertyAgentController::class, 'unassign']);
+
+Route::get('/agents', [AgentController::class, 'index']);
