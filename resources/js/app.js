0d14559ca0a1141a46ca89ebc06c6e6f7282491a
imurar <imurar@outlook.com>
2025-07-27 21:06:45 +0900

    refactor: add agent info

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,12 @@
 import "leaflet/dist/leaflet.css";
 import { createApp } from "vue";
 import "./bootstrap";
+import AgentAssign from "./components/AgentAssign.vue";
 import PropertyMap from "./components/PropertyMap.vue";
 
 const app = createApp({});
 app.component("property-map", PropertyMap);
+app.component("agent-assign", AgentAssign);
+
 app.mount("#vue-property-map");
+app.mount("#vue-agent-assign");
