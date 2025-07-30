246357b59ba5a679ce716ee77fe9a94259451169
imurar <imurar@outlook.com>
2025-07-31 08:57:43 +0900

    fix: agents edit.blade append email

--- resources/views/agents/edit.blade.php
+++ resources/views/agents/edit.blade.php
@@ -3,15 +3,17 @@
 @section('content')
 <h1>担当者編集</h1>
 
-<form action="{{ route('agent.update', $agent->id }}" method="POST">
+<form action="{{ route('agents.update', $agent->id) }}" method="POST">
     @csrf
     @method('PUT')
     <div class="mb-3">
         <label>名前</label>
-        <input type="text" name="name" class="btn-control" value="{{ $agent->name }}" required>
+        <input type="text" name="name" class="form-control" value="{{ $agent->name }}" required>
     </div>
     <div class="mb-3">
         <label>Email</label>
-        <input type="text">
+        <input type="email" name="email" class="form-control" value="{{ $agent->email }}" required>
     </div>
+    <button type="submit" class="btn btn-primary">更新</button>
 </form>
+@endsection
