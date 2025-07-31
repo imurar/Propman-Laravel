da3038306958d6fbcd1af980eda947ca4a0e03da
imurar <imurar@outlook.com>
2025-07-31 12:55:19 +0900

    fix: routing-restfu

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\AgentController;
 
-// Inertiaページとしてルーティング
-Route::get('/', [PropertyController::class, 'index'])->name('home');
-Route::resource('properties', PropertyController::class)->except(['show']);
-Route::get('properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
-Route::resource('agents', AgentController::class)->except(['show']);
-Route::get('agents/{agent}', [AgentController::class, 'show'])->name('agents.show');
+Route::resource('agents', AgentController::class);
+Route::resource('properties', PropertyController::class);
