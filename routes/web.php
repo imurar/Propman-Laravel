38cb8a75559aec0e05e230b0d120ccd0c025413c
imurar <imurar@outlook.com>
2025-07-31 09:37:48 +0900

    refactor: routeing-inertia.js

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\AgentController;
 
-// 物件一覧・登録・編集・削除
-Route::resource('properties', PropertyController::class);
+// Inertiaページとしてルーティング
+Route::get('/', [PropertyController::class, 'index'])->name('home');
+Route::resource('properties', PropertyController::class)->except(['show']);
+Route::get('properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
-// 担当者一覧・登録・編集・削除
-Route::resource('agents', AgentController::class);
-
-// トップページを物件一覧にリダイレクト
-Route::get('/', function () {
-    return redirect()->route('properties.index');
-});
+Route::resource('agents', AgentController::class)->except(['show']);
+Route::get('agents/{agent}', [AgentController::class, 'show'])->name('agents.show');
